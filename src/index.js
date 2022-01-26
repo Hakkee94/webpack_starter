aa3558c9432ba5ef582dba1a0f84fa9310a067ac
Hakkee94 <agasyandavid94@gmail.com>
2022-01-26 14:35:10 +0300

    webpack starter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
+import Dragon from './shared/images/Dragon.svg'
+import baloons from './shared/images/balloons.png'
+import './style.scss'
+
+class WorldName {
+    #name
+
+    constructor() {
+        this.#name = ''
+    }
+
+    setName() {
+        this.#name = prompt("Please write your name")
+    }
+
+    getName() {
+        return this.#name
+    }
+}
+
+const myName = new WorldName()
+myName.setName()
+
+
 const heading = document.createElement('h1')
-heading.textContent = 'Hello world!'
+heading.textContent = 'Hello world!' + myName.getName()
+
+const img = document.createElement('img')
+img.src = Dragon
+
+const baloonsImg = document.createElement('img')
+baloonsImg.src = baloons
+
 
 const root = document.querySelector('#root')
 root.append(heading)
+root.append(img)
+root.append(baloonsImg)
+
